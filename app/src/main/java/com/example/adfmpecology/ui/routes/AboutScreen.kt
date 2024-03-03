3c853e6058f0eeec61177f954ef072130d5953aa
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2024-03-04 00:18:38 +0300

    feat: add aboutscreen

---
+++ app/src/main/java/com/example/adfmpecology/ui/routes/AboutScreen.kt
@@ -0,0 +1,24 @@
+package com.example.adfmpecology.ui.routes
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.navigation.NavHostController
+
+
+@Composable
+fun AboutScreen(
+    navController: NavHostController,
+    modifier: Modifier = Modifier
+) {
+    
+}
+
