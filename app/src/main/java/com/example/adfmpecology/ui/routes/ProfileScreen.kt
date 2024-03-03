813afe05b2f6831810875d73ac6cd52566db3cce
Vlad <vladbolkunovv@gmail.com>
2024-03-03 14:55:41 +0300

    fix(ProfileScreen): UserData import

--- app/src/main/java/com/example/adfmpecology/ui/routes/ProfileScreen.kt
+++ app/src/main/java/com/example/adfmpecology/ui/routes/ProfileScreen.kt
@@ -13,8 +13,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.adfmpecology.data.types.UserData
 import com.example.adfmpecology.ui.components.user.AuthCard
-import com.example.adfmpecology.ui.components.user.UserData
 import com.example.adfmpecology.ui.components.user.UserProfile
 
 @Composable
