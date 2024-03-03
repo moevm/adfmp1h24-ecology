8fc3c2d819cde0c4f9fc33490a794a94d3128b35
Давыдов Михаил <71800457+dart-mih@users.noreply.github.com>
2024-03-04 00:18:03 +0300

    feat: add map

--- app/src/main/java/com/example/adfmpecology/ui/routes/MapScreen.kt
+++ app/src/main/java/com/example/adfmpecology/ui/routes/MapScreen.kt
@@ -1,11 +1,33 @@
 package com.example.adfmpecology.ui.routes
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import com.google.android.gms.maps.model.CameraPosition
+import com.google.android.gms.maps.model.LatLng
+import com.google.maps.android.compose.GoogleMap
+import com.google.maps.android.compose.rememberCameraPositionState
 
 
 @Composable
-fun MapScreen(modifier: Modifier = Modifier) {
-    Text(text = "Конфетка, а не карта")
+fun MapScreen(
+) {
+    // Set properties using MapProperties which you can use to recompose the map
+
+    val singapore = LatLng(1.35, 103.87)
+    val cameraPositionState = rememberCameraPositionState {
+        position = CameraPosition.fromLatLngZoom(singapore, 10f)
+    }
+    Box(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        GoogleMap(
+            modifier = Modifier.fillMaxSize(),
+            cameraPositionState = cameraPositionState
+        )
+
+    }
 }
+
+
