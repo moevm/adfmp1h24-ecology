5b90781eebe56d5337eb7df7dd27a8d935398fc0
Vlad <vladbolkunovv@gmail.com>
2024-03-07 00:48:10 +0300

    feat: ecology api

---
+++ app/src/main/java/xyz/moevm/ecology/api/api.kt
@@ -0,0 +1,27 @@
+package xyz.moevm.ecology.api
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+
+const val SERVER_URL = "https://vladdoth.xyz/ecology/api/"
+
+
+private val retrofit = Retrofit.Builder()
+    .addConverterFactory(GsonConverterFactory.create())
+    .client(
+        OkHttpClient()
+            .newBuilder()
+            .cookieJar(SessionCookieJar())
+            .build()
+    )
+    .baseUrl(SERVER_URL)
+    .build()
+
+
+object EcologyApi {
+    val auth: EcologyAuthApiService by lazy {
+        retrofit.create(EcologyAuthApiService::class.java)
+    }
+}
