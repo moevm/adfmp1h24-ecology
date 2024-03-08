b1a036f63f5052ff28eb861ecf993e078f31811c
Vlad <vladbolkunovv@gmail.com>
2024-03-09 01:03:50 +0300

    feat: datastore deps

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -73,7 +73,8 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-
+    implementation("androidx.datastore:datastore:1.0.0")
+    implementation("androidx.datastore:datastore-preferences:1.0.0")
 
     testImplementation("junit:junit:4.13.2")
 
