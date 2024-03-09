7b5c8c31fe5a691db297bb531d183cd462ae4b4f
Vlad <vladbolkunovv@gmail.com>
2024-03-09 14:40:43 +0300

    refactor(api): viewmodel api

--- app/src/main/java/xyz/moevm/ecology/api/api.kt
+++ app/src/main/java/xyz/moevm/ecology/api/api.kt
@@ -1,27 +1,30 @@
 package xyz.moevm.ecology.api
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import xyz.moevm.ecology.api.services.EcologyAuthApiService
-
+import xyz.moevm.ecology.data.stores.CookiesViewModel
 
 const val SERVER_URL = "https://vladdoth.xyz/ecology/api/"
 
+class ApiViewModel(application: Application) : AndroidViewModel(application) {
+    private val cookiesVM = CookiesViewModel(application)
+    private val cookieJar = DataStoreCookieJar(cookiesVM)
 
-private val retrofit = Retrofit.Builder()
-    .addConverterFactory(GsonConverterFactory.create())
-    .client(
-        OkHttpClient()
-            .newBuilder()
-            .cookieJar(SessionCookieJar())
-            .build()
-    )
-    .baseUrl(SERVER_URL)
-    .build()
+    private val retrofit = Retrofit.Builder()
+        .addConverterFactory(GsonConverterFactory.create())
+        .client(
+            OkHttpClient()
+                .newBuilder()
+                .cookieJar(cookieJar)
+                .build()
+        )
+        .baseUrl(SERVER_URL)
+        .build()
 
-
-object EcologyApi {
     val auth: EcologyAuthApiService by lazy {
         retrofit.create(EcologyAuthApiService::class.java)
     }
