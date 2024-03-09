d866831b7fc9407293a287ae15d10becd0f413cd
Vlad <vladbolkunovv@gmail.com>
2024-03-09 14:40:11 +0300

    feat: datastore cookiesjar

--- app/src/main/java/xyz/moevm/ecology/api/cookies.kt
+++ app/src/main/java/xyz/moevm/ecology/api/cookies.kt
@@ -1,17 +1,24 @@
 package xyz.moevm.ecology.api
 
+import kotlinx.coroutines.runBlocking
 import okhttp3.Cookie
 import okhttp3.CookieJar
 import okhttp3.HttpUrl
+import xyz.moevm.ecology.data.stores.CookiesViewModel
 
-class SessionCookieJar : CookieJar {
-    var cookies: List<Cookie> = emptyList()
-
+class DataStoreCookieJar(private val cookiesVM: CookiesViewModel) : CookieJar {
     override fun saveFromResponse(url: HttpUrl, cookies: List<Cookie>) {
-        this.cookies = ArrayList(cookies)
+        runBlocking {
+            cookiesVM.store.setCookies(cookies)
+        }
     }
 
     override fun loadForRequest(url: HttpUrl): List<Cookie> {
-        return cookies
+        return runBlocking {
+            return@runBlocking cookiesVM.store
+                .getCookies()
+                ?.map { Cookie.parse(url, it) }
+                ?.filterNotNull()
+        } ?: emptyList()
     }
 }
