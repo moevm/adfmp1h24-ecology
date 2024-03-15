5db86f3c769e62cf9cea205d6897df8aa7d7d7ac
Vlad <vladbolkunovv@gmail.com>
2024-03-15 15:02:33 +0300

    feat(UsersViewModel): current user state & karma update

--- app/src/main/java/xyz/moevm/ecology/data/viewmodels/UsersViewModel.kt
+++ app/src/main/java/xyz/moevm/ecology/data/viewmodels/UsersViewModel.kt
@@ -3,6 +3,7 @@ package xyz.moevm.ecology.data.viewmodels
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
@@ -15,15 +16,47 @@ class UsersViewModel(application: Application) : AndroidViewModel(application) {
     private val api = ApiViewModel(application)
     private val karmaMock = KarmaMockViewModel(application)
 
-    private val _state = MutableStateFlow<List<ServerUserData>>(emptyList())
-    val state = _state.asStateFlow()
+    private val usersListState = MutableStateFlow<List<ServerUserData>>(emptyList())
+    val usersList = usersListState.asStateFlow()
 
-    fun fetchUsers() {
-        viewModelScope.launch {
-            _state.update {
+    fun fetchUsers(): Job {
+        return viewModelScope.launch {
+            usersListState.update {
                 api.users.getUsersList().body()?.map {
                     it.copy(karma = karmaMock.store.getUserKarma(it._id!!.id!!))
                 } ?: emptyList()
+            }
+        }
+    }
+
+    private val currentUserState = MutableStateFlow<ServerUserData?>(null)
+    val currentUser = currentUserState.asStateFlow()
+
+    suspend fun fetchUser(id: String) = api.users.getUser(id).body()?.copy(
+        karma = karmaMock.store.getUserKarma(id)
+    )
+
+    fun fetchCurrentUser(id: String): Job {
+        return viewModelScope.launch {
+            currentUserState.update {
+                fetchUser(id)
+            }
+        }
+    }
+
+    suspend fun updateUserKarma(user: ServerUserData, value: Int): ServerUserData {
+        val karma = karmaMock.store.getUserKarma(user._id!!.id!!)
+        karmaMock.store.setUserKarma(
+            user._id.id!!, karma + value
+        )
+
+        return user.copy(karma = karma + value)
+    }
+
+    fun updateCurrentUserKarma(value: Int): Job {
+        return viewModelScope.launch {
+            currentUserState.update {
+                updateUserKarma(currentUserState.value!!, value)
             }
         }
     }
