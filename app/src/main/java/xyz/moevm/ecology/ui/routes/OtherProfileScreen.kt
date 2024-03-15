718681e06e13de4a962b3afe0b9489834c9c83d9
Vlad <vladbolkunovv@gmail.com>
2024-03-15 15:47:10 +0300

    fix(OtherProfile): card fill

--- app/src/main/java/xyz/moevm/ecology/ui/routes/OtherProfileScreen.kt
+++ app/src/main/java/xyz/moevm/ecology/ui/routes/OtherProfileScreen.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
@@ -71,7 +72,7 @@ fun OtherProfileScreen(
             )
         }
 
-        Card(modifier) {
+        Card(modifier.fillMaxSize()) {
             Row(
                 modifier = Modifier.fillMaxWidth(),
                 Arrangement.Absolute.SpaceAround,
